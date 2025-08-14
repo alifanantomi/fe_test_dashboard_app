1e2771a4274b95515a719ded8e6c57391160299c
alifanantomi <alifanantomi2@gmail.com>
2025-08-14 15:47:29 +0700

    feat(router): configure routing with route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,42 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import DashboardView from '@/views/DashboardView.vue';
+import LoginView from '@/views/LoginView.vue';
+import store from '@/store';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    redirect: '/login',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    meta: { requiresAuth: false },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const { isAuthenticated } = store.state.auth;
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login');
+  } else if (to.name === 'Login' && isAuthenticated) {
+    next('/dashboard');
+  } else {
+    next();
+  }
 });
 
 export default router;
